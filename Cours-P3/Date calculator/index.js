90455e2d472f8782fa3ea0774da18032fcd60e61
adrien magnies <amagnies@axe.local>
2022-10-10 13:22:30 +0200

    Envoi de l'exercice date calculator (fin)

--- Cours-P3/Date calculator/index.js
+++ Cours-P3/Date calculator/index.js
@@ -30,3 +30,16 @@ end_date.addEventListener("change", (e) => {
         start_date.value = day.toISOString().split("T")[0];
     }
 });
+
+const bookingCalc = () => {
+    let diffTime = Math.abs(new Date(end_date.value) - new Date(start_date.value));
+
+    let diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+    
+    totalPerNight.textContent = diffDays * nightPrice.textContent;
+};
+
+start_date.addEventListener("change", bookingCalc);
+end_date.addEventListener("change", bookingCalc);
+
+bookingCalc();
